a2779437dc2854f762f130cba5d97ff1a16fb338
luanhrods <84470000+luanhrods@users.noreply.github.com>
2024-10-20 13:29:23 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        nunito: ['Nunito Sans', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+        'float-delay-1': 'float 6s  ease-in-out 1s infinite',
+        'float-delay-2': 'float 6s ease-in-out 2s infinite',
+        'float-delay-3': 'float 6s ease-in-out 3s infinite',
+        'float-delay-4': 'float 6s ease-in-out 4s infinite',
+        'float-delay-5': 'float 6s ease-in-out 5s infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        }
+      }
+    },
+  },
+  // ... other configurations
+}
