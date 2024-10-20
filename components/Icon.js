b73c2b8fcf33f0c9bc126c3aee08f7ef13c883b0
luanhrods <84470000+luanhrods@users.noreply.github.com>
2024-10-20 13:37:59 -0400

    Update Icon.js

--- components/Icon.js
+++ components/Icon.js
@@ -1,1 +1,16 @@
+import { Play, Instagram, Mail, Youtube, Smartphone, PenTool, Video } from 'lucide-react'
 
+const iconMap = {
+  Play,
+  Instagram,
+  Mail,
+  Youtube,
+  Smartphone,
+  PenTool,
+  Video,
+}
+
+export default function Icon({ name, ...props }) {
+  const IconComponent = iconMap[name]
+  return IconComponent ? <IconComponent {...props} /> : null
+}
