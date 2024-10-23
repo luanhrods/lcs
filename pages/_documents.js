ce797f12cf642f286bebdaf327b3367e8a93a74f
luanhrods <84470000+luanhrods@users.noreply.github.com>
2024-10-23 08:15:19 -0400

    Create _documents.js

---
+++ pages/_documents.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
